769d2acef2e5413e3a6fe54601e8b923fa11d415
Mustafa Joda <juda2018@gmail.com>
2026-06-15 15:37:17 +0300

    disable notification button temporarily

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
  import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import NotificationPermission from "./components/NotificationPermission";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,10 +30,6 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-black">
-        <NotificationPermission
-          userType="admin"
-          userId="fuse-admin"
-        />
         {children}
       </body>
     </html>
