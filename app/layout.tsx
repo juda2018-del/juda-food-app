4e818c063c9289af822c5f42a983eeb6535eada5
Mustafa Joda <juda2018@gmail.com>
2026-06-15 15:08:48 +0300

    fix notification component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
  import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NotificationPermission from "./components/NotificationPermission";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-black">
+        <NotificationPermission
+          userType="admin"
+          userId="fuse-admin"
+        />
         {children}
       </body>
     </html>
