0602974c3f2c61a8762ddf35b827f19e544d68e6
Mustafa Joda <juda2018@gmail.com>
2026-06-29 13:11:03 +0300

    Fix login redirect and protect dashboards

---
+++ lib/fuseAuthClient.ts
@@ -0,0 +1,7 @@
+﻿import "./firebase";
+import { getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const app = getApps()[0];
+
+export const fuseAuth = app ? getAuth(app) : getAuth();
