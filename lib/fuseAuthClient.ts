be2827d6aa9429bb0bd24c3d472c175d006f9a5a
Mustafa Joda <juda2018@gmail.com>
2026-06-29 13:33:22 +0300

    Fix Firebase duplicate app and enforce real login

--- lib/fuseAuthClient.ts
+++ lib/fuseAuthClient.ts
@@ -1,15 +1,4 @@
-import { getApp, getApps, initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB8sjJEn2meAPdYDsLn9RjLoQ3d51dsqa0",
-  authDomain: "juda-food-app.firebaseapp.com",
-  projectId: "juda-food-app",
-  storageBucket: "juda-food-app.firebasestorage.app",
-  messagingSenderId: "309377324974",
-  appId: "1:309377324974:web:0ee974f1cb046f04281a3f",
-};
-
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+﻿import { getAuth } from "firebase/auth";
+import { app } from "../app/firebase";
 
 export const fuseAuth = getAuth(app);
