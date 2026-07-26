448159f951f41ffae16a6af2cb925f6a09e447bf
juda2018-del <juda2018@gmail.com>
2026-07-26 15:11:08 +0300

    Keep Firebase login session keys synchronized

---
+++ app/login/SessionStorageBridge.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { useEffect } from "react";
+import { FUSE_LOCAL_SESSION } from "@/lib/fuse-auth";
+
+const LEGACY_SESSION_KEY = "FUSE_LOCAL_SESSION";
+
+export default function SessionStorageBridge() {
+  useEffect(() => {
+    const originalSetItem = Storage.prototype.setItem;
+
+    Storage.prototype.setItem = function patchedSetItem(key: string, value: string) {
+      originalSetItem.call(this, key, value);
+
+      if (this === window.localStorage && key === LEGACY_SESSION_KEY && FUSE_LOCAL_SESSION !== LEGACY_SESSION_KEY) {
+        originalSetItem.call(this, FUSE_LOCAL_SESSION, value);
+      }
+
+      if (this === window.localStorage && key === FUSE_LOCAL_SESSION && FUSE_LOCAL_SESSION !== LEGACY_SESSION_KEY) {
+        originalSetItem.call(this, LEGACY_SESSION_KEY, value);
+      }
+    };
+
+    const legacy = window.localStorage.getItem(LEGACY_SESSION_KEY);
+    const current = window.localStorage.getItem(FUSE_LOCAL_SESSION);
+
+    if (legacy && !current) {
+      originalSetItem.call(window.localStorage, FUSE_LOCAL_SESSION, legacy);
+    } else if (current && !legacy) {
+      originalSetItem.call(window.localStorage, LEGACY_SESSION_KEY, current);
+    }
+
+    return () => {
+      Storage.prototype.setItem = originalSetItem;
+    };
+  }, []);
+
+  return null;
+}
