c94b77ed886eb4c55d1c0510979508da4176b3c2
juda2018-del <juda2018@gmail.com>
2026-07-26 14:04:49 +0300

    Use clean menu flow for Al Forn

--- app/restaurants/alforn/page.tsx
+++ app/restaurants/alforn/page.tsx
@@ -1,5 +1,5 @@
-﻿import RestaurantOrderClient from "../RestaurantOrderClient";
+import RestaurantMenuClient from "../RestaurantMenuClient";
 
 export default function Page() {
-  return <RestaurantOrderClient restaurant="الفرن" />;
+  return <RestaurantMenuClient restaurant="الفرن" restaurantId="alforn" />;
 }
